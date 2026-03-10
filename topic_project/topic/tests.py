0cd82c287d2a252ae49ed9b7f26196c8e557590a
Jacob Liu <3070970l@student.gla.ac.uk>
2026-03-10 21:10:20 +0000

    moved tests to test.py

--- topic_project/topic/tests.py
+++ topic_project/topic/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+# Using the templates from "https://github.com/tangowithcode/tango_with_django_2_code/blob/master/progress_tests/"
 
-# Create your tests here.
+
+import os
+import warnings
+import importlib
+from django.urls import reverse
+from django.test import TestCase
+from selenium import webdriver
+from django.contrib.auth.models import User
+
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}================{os.linesep}TwD TEST FAILURE =({os.linesep}================{os.linesep}"
+FAILURE_FOOTER = f"{os.linesep}"
+
+class CloseButtonTests(TestCase):
+    def setUp(self):
+        pass
+
+    def close_announcement(self):
+        pass
