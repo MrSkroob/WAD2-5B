6ed0d1e0215b02d01a5d2097a09641673181d96f
Jacob Liu <3070970l@student.gla.ac.uk>
2026-03-10 14:23:45 +0000

    TODO and remove redundant if statement

--- topic_project/static/js/close_announcement.js
+++ topic_project/static/js/close_announcement.js
@@ -2,12 +2,11 @@
 let announcement = document.getElementById("announcements")
 // we only need to worry about one button 
 let closeButton = announcement.getElementsByTagName("button")[0]
+
+// TODO: remember user's preference, but show the announcement again if there's a new one. 
+
 let defaultDisplay = announcement.style.display
 
 closeButton.addEventListener("click", () => {
-    if (announcement.style.display === "none") {
-        announcement.style.display = defaultDisplay;
-    } else {
-        announcement.style.display = "none";
-  }
+    announcement.style.display = "none";
 })
